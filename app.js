d87b5c8ee5befeb2471395dce3cb7bf0fbe082e2
TaikiFnit <taikifnitspg@gmail.com>
2016-07-09 22:46:19 +0900

    change option for heroku

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const url = require('url');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'safe-fortress-15244.herokuapp.com';
+const port = 80;
 
 const makeRequest = (host, path)=> {
   return new Promise((resolve, reject) => {
